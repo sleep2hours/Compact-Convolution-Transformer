08f5d27042a5443ff806012a203f500a3f625514
sleep2hours <1569171127@qq.com>
2022-04-08 23:00:57 +0800

    Merge branch 'master' of github.com:sleep2hours/Compact-Convolution-Transformer

--- CCT.py
+++ CCT.py
@@ -12,10 +12,16 @@ class CCT(nn.module):
         super(CCT, self).__init__()
         self.conv_num=args.conv_num
         self.kernel_size=args.kernel_size
-        self.encoder_num=args.encoder_num
-        
-        self.layer1=copy_layer(ConvEmbed,self.conv_num)
+        self.mids=args.mids
+        self.outputs=args.outputs
+        self.encoder_num = args.encoder_num
+        self.layer1=ConvEmbed(self.kernel_size,self.mids,self.outputs,self.conv_num)
+        self.layer2=copy_layer(Transformer,self.encoder_num)
         
 class ConvEmbed(nn.module):
-    def __init__(self):
+    def __init__(self,kernel_size,mids,outputs,conv_num):
         super(ConvEmbed, self).__init__()
+
+class Transformer(nn.module):
+    def __init__(self,h,d,dropout=0.1):
+        super(Transformer, self).__init__()
